493ad9de625fd224df21e275e92199445aea575a
bighneswar senapati <bignesh2809@gmail.com>
2026-02-21 20:28:01 +0530

    test

---
+++ python niis/BASIC/trapezium.py
@@ -0,0 +1,12 @@
+print("enter trapezium 1st parallel sides")
+a=int(input())
+b=int(input())
+print("enter trapezium 2nd parallel sides")
+c=int(input())
+d=int(input())
+print("enter trapezium height")
+h=int(input())
+ar=1/2*a+b*h
+pr=a+b+c+d
+print("area=",ar)
+print("perimeter=",pr)
